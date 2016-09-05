d6e9a91abb309df5ada4dbd3f018439fbcf539e4
atharva <atarw@users.noreply.github.com>
2016-09-05 19:41:32 -0400

    built static files

--- src/site.py
+++ src/site.py
@@ -1,5 +1,8 @@
+import sys
+
 from flask import Flask, render_template
 from flask_flatpages import FlatPages
+from flask_frozen import Freezer
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
@@ -8,6 +11,7 @@ FLATPAGES_EXTENSION = '.md'
 app = Flask (__name__)
 app.config.from_object (__name__)
 pages = FlatPages (app)
+freezer = Freezer (app)
 
 '''
 Site Structure:
@@ -52,4 +56,7 @@ def tag (tag):
 	return render_template ('tags.html', pages = tagged, tag = tag)
 
 if __name__ == '__main__':
-	app.run (port = 8000)
+	if len (sys.argv) > 1 and sys.argv [1] == 'build':
+		freezer.freeze ()
+	else:
+		app.run (port = 8000)
